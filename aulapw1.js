6a3d37ad3b3ed8d09bb108215df55720d070d324
levixz <151973492+levixz@users.noreply.github.com>
2023-11-24 20:07:42 -0300

    Add files via upload

---
+++ aulapw1.js
@@ -0,0 +1,88 @@
+//bloco 1
+function bt1bloco1cor1() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'purple';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'purple';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'purple';
+    document.getElementById('secao4lin1col1').style.backgroundColor = 'purple';
+}
+
+
+function bt1bloco1cor2() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'yellow';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'yellow';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'yellow';
+    document.getElementById('secao4lin1col1').style.backgroundColor = 'yellow';
+
+}
+
+function bt1bloco1cor3() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'blue';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'blue';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'blue';
+    document.getElementById('secao4lin1col1').style.backgroundColor = 'blue';
+}
+
+function bt1bloco1cor4() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'red';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'red';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'red';
+    document.getElementById('secao4lin1col1').style.backgroundColor = 'red';
+}
+
+//bloco 2
+function bt1bloco2cor1() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'purple';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'purple';
+    document.getElementById('secao4lin1col2').style.backgroundColor = 'purple';
+}
+
+function bt1bloco2cor2() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'yellow';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'yellow';
+    document.getElementById('secao4lin1col2').style.backgroundColor = 'yellow';
+}
+
+function bt1bloco2cor3() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'blue ';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'blue ';
+    document.getElementById('secao4lin1col2').style.backgroundColor = 'blue ';
+}
+
+function bt1bloco2cor4() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'red';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'red';
+    document.getElementById('secao4lin1col2').style.backgroundColor = 'red';
+}
+
+
+//bloco 3
+function bt1bloco3cor1() {
+    document.getElementById('secao3lin1col3').style.backgroundColor = 'purple';
+    document.getElementById('secao4lin1col3').style.backgroundColor = 'purple';
+}
+function bt1bloco3cor2() {
+    document.getElementById('secao3lin1col3').style.backgroundColor = 'yellow';
+    document.getElementById('secao4lin1col3').style.backgroundColor = 'yellow';
+}
+function bt1bloco3cor3() {
+    document.getElementById('secao3lin1col3').style.backgroundColor = 'blue';
+    document.getElementById('secao4lin1col3').style.backgroundColor = 'blue';
+}
+function bt1bloco3cor4() {
+    document.getElementById('secao3lin1col3').style.backgroundColor = 'red';
+    document.getElementById('secao4lin1col3').style.backgroundColor = 'red';
+}
+
+//bloco 4
+function bt1bloco4cor1() {
+    document.getElementById('secao4lin1col4').style.backgroundColor = 'purple';
+}
+function bt1bloco4cor2() {
+    document.getElementById('secao4lin1col4').style.backgroundColor = 'yellow';
+}
+function bt1bloco4cor3() {
+    document.getElementById('secao4lin1col4').style.backgroundColor = 'blue';
+}
+function bt1bloco4cor4() {
+    document.getElementById('secao4lin1col4').style.backgroundColor = 'red';
+}
